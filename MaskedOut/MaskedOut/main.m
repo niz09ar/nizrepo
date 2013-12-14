228dcbd43717848d5e4aa7fc7ba46a7190446126
Nizar <nizarcher@hotmail.com>
2013-12-13 22:27:40 -0500

    mask

---
+++ MaskedOut/MaskedOut/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MaskedOut
+//
+//  Created by nizar cherkaoui on 12/12/13.
+//  Copyright (c) 2013 nizar cherkaoui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
